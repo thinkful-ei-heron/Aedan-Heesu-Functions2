6ae0d8a7d8c1aba4489c636f87d6a6f063cc2eb5
Just-A-Fool <aedanscarn@gmail.com>
2019-09-16 14:16:59 -0600

    decoder added

--- functions2.js
+++ functions2.js
@@ -27,9 +27,24 @@ function beyond(num) {
   }
 }
 
-
-
 beyond (3);
 beyond (0);
 beyond (-5);
 beyond (Infinity);
+
+function decode(str) {
+  let subArray = str.split(' ');
+  let word = '';
+  for (let i = 0; i < subArray.length; i++){
+    let first = subArray[i].charAt(0);
+    if (first === 'a') word += subArray[i].charAt(1);
+    else if (first === 'b') word += subArray[i].charAt(2);
+    else if (first === 'c') word += subArray[i].charAt(3);
+    else if (first === 'd') word += subArray[i].charAt(4);
+    else word += ' ';
+  }
+  return word;
+}
+
+console.log(decode('craft block argon meter bells brown croon droop'));
+
